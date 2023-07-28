0a3128a8f3ea3d2c1c8b116ef44eabe6c07f717e
RicGrimaldo <rgrimaldo2307@gmail.com>
2023-07-27 23:18:13 -0600

    Order and search currencies functions implemented

--- src/pages/Currencies.tsx
+++ src/pages/Currencies.tsx
@@ -1,13 +1,58 @@
 import { IconCoin } from "@tabler/icons-react";
-import { DropdownOrderBy, Header, SearchInput } from "../components";
-
-//TODO Renderizar arreglo de currencies
-//TODO Crear condicion en caso de que no haya datos
-//TODO Hacer el ordenado
-//TODO Crear handleDropdown para ordenar
-//TODO Crear handleSearch filtrar el arreglo
+import { Currency, DropdownOrderBy, Header, SearchInput } from "../components";
+import { Currency as ICurrency } from "../interfaces";
+import { useEffect, useState } from "react";
+import { currenciesMock } from "../mocks";
 
 export const Currencies = () => {
+
+	const [currencies, setCurrencies] = useState<ICurrency[]>([]);
+	const [currentOrderOption, setCurrentOrderOption] = useState("name"); 
+
+	const orderOptions: { label: string; value: string }[] =
+	[
+		{ label: "Nombre", value: "name" },
+		{ label: "Cambio", value: "value" },
+	];
+
+	useEffect(() => {
+		setCurrencies(currenciesMock);
+		setCurrencies((prevState) => orderCurrencies(prevState, currentOrderOption));
+	}, [])
+
+	const orderCurrencies = (currencies: ICurrency[], currentOrderOption: string): ICurrency[]=>{
+		let key = currentOrderOption as keyof (typeof currencies)[0];
+		const newCurrencies: ICurrency[] = currencies.sort(
+			(a: ICurrency, b: ICurrency) => {
+				if (a[key] > b[key]) return 1;
+				if (a[key] < b[key]) return -1;
+				return 0;
+			}
+		)
+		return newCurrencies;
+	};
+
+	const handleDropdown = (e: React.ChangeEvent<HTMLSelectElement>) => {
+		setCurrentOrderOption(e.target.value);
+		setCurrencies(orderCurrencies(currencies,e.target.value));
+	};
+
+	const handleSearch = (searchWord: string) => {
+		if (searchWord === "") {
+			setCurrencies(currenciesMock);		
+		} else {
+			let newCurrencies = currenciesMock.filter((currency) => {
+				searchWord = searchWord.toLowerCase();
+				if (currency.value.toString().toLowerCase().indexOf(searchWord) !== -1
+					|| currency.name.toLowerCase().indexOf(searchWord) !== -1
+					|| currency.symbol.toLowerCase().indexOf(searchWord) !== -1) {
+					return currency;
+				}
+			});
+			setCurrencies(newCurrencies);
+		}
+	};
+
 	return (
 		<>
 			<Header>
@@ -16,13 +61,13 @@ export const Currencies = () => {
 				</h1>
 				<div className="flex w-full gap-2 sm:w-96">
 					<DropdownOrderBy
-						onChange={console.log}
-						options={[]}
-						value={""}
+						onChange={handleDropdown}
+						options={orderOptions}
+						value={currentOrderOption}
 					/>
 					<SearchInput
 						Icon={IconCoin}
-						onSearch={console.log}
+						onSearch={(e) => handleSearch(e.target.value)}
 						propertie="divisa"
 					/>
 				</div>
@@ -32,7 +77,23 @@ export const Currencies = () => {
 				<ul
 					role="list"
 					className="grid w-full gap-3 overflow-auto divide-y divide-gray-100 sm:grid-cols-2 xl:grid-cols-4 my-7"
-				></ul>
+				>
+					{ 
+					
+					currencies.length === 0 ? (<div className="flex flex-col items-center justify-center h-full">
+					<p className="text-3xl font-bold text-center">
+						¡Oh no! :(
+					</p>
+					<p className="mt-5 text-lg text-center">
+						Algo no ha salido como esperabamos. Por favor,
+						intentalo más tarde.
+					</p>
+				</div>) : 
+					(currencies.map((currency, index)=>{
+							return <Currency currency={currency} key={index}/>;
+						}))
+					}
+				</ul>
 			</section>
 		</>
 	);
